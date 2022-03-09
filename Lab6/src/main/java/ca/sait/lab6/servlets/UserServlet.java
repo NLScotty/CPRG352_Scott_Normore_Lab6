1398b8bb494fbdc7d502a9f0fb7599151c9360a5
sbn073 <Scott.B.Normore@gmail.com>
2022-03-09 13:43:14 -0700

    First Commit
    Doing this for backup Reasons.

---
+++ Lab6/src/main/java/ca/sait/lab6/servlets/UserServlet.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package ca.sait.lab6.servlets;
+
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import ca.sait.lab6.services.RoleService;
+import ca.sait.lab6.services.UserService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Scott
+ */
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserService userService = new UserService();
+        RoleService roleService = new RoleService();
+
+        try{
+            List<User> users = userService.getAll();
+            List<Role> role = roleService.getAll();
+            
+            request.setAttribute("users",users);
+            
+        } catch(Exception ex){
+            System.out.println(ex);
+        }
+        this.getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+    }
+}
---
+++ Lab6/target/Lab6-1.0-SNAPSHOT/WEB-INF/users.jsp
@@ -0,0 +1,39 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        
+    </head>
+    <body>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>E-mail</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Role Name</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <c:forEach items="${users}" var="user">
+                    <tr>
+                        <td>${user.email}</td>
+                        <td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.role.name}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </c:forEach>
+            </tbody>
+        </table>
+    </body>
+</html>
